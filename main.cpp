b3fb2381563896276de88bcbf60fb2619a08bf6e
ura-create <uulinnik200451@gmail.com>
2021-04-24 22:47:56 +0300

    Add refference

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+    int a = 8;
+    int *ptr = &a;
+    *ptr = 2;
+    std::cout<<*ptr<<std::endl;
+    int &ref = a;
+    a = 5;
+    std::cout<<a<<std::endl;
+}
